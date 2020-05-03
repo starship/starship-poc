15641c6badafcf45ab03f27f767750bd03dfc16a
github-action-benchmark <github@users.noreply.github.com>
2020-05-03 15:59:12 +0000

    add Rust Benchmark (cargo) benchmark result for 7b7a4ba9d81592b5500498be06ad873eace0d688

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1588521552747,
+  "repoUrl": "https://github.com/matchai/starship-poc",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@matchai.dev",
+            "name": "Matan Kushner",
+            "username": "matchai"
+          },
+          "committer": {
+            "email": "hello@matchai.dev",
+            "name": "Matan Kushner",
+            "username": "matchai"
+          },
+          "distinct": true,
+          "id": "7b7a4ba9d81592b5500498be06ad873eace0d688",
+          "message": "Add a basic benchmark",
+          "timestamp": "2020-05-03T11:57:05-04:00",
+          "tree_id": "ed8f91b3c2340d91e3cb13b8c217b2a60e70922a",
+          "url": "https://github.com/matchai/starship-poc/commit/7b7a4ba9d81592b5500498be06ad873eace0d688"
+        },
+        "date": 1588521552266,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_render",
+            "value": 2159730,
+            "range": "± 425093",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
